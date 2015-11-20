f93d9b60f99507307cf97ffb76bcb3271354155f
m.malkin <mmalkin@fun-box.ru>
2015-11-20 14:48:00 +0300

    install script fixed

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm
-sudo apt-get update
-sudo apt-get install rcm
-
 sudo apt-get install zsh
-chsh -s $(which zsh)
-
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 gem install pry
@@ -14,4 +8,9 @@ gem install awesome_print
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
+wget https://thoughtbot.github.io/rcm/debs/rcm_1.3.0-1_all.deb
+sudo dpkg -i rcm_1.3.0-1_all.deb
+
 rcup -U .oh-my-zsh/custom/plugins/pj -U scripts -U bin -x install
+
+echo 'export PROJECTS=$HOME/projects' > ~/.zsh_local
