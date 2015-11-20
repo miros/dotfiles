f307261237d453634963c29470058fec2875a04d
m.malkin <mmalkin@fun-box.ru>
2015-11-20 14:59:14 +0300

    rbenv added to install script

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -2,11 +2,14 @@
 sudo apt-get install zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-gem install pry
-gem install awesome_print
-
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+rbenv install 2.2.3
+gem install bundler pry awesome_print
 
 wget https://thoughtbot.github.io/rcm/debs/rcm_1.3.0-1_all.deb
 sudo dpkg -i rcm_1.3.0-1_all.deb
