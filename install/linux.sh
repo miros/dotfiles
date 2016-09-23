77723c04b8dc3e4aa38ecf35377d3d59b9252e23
m.malkin <mmalkin@fun-box.ru>
2016-09-24 00:08:35 +0300

    typo fixed

--- install/linux.sh
+++ install/linux.sh
@@ -3,7 +3,10 @@ set -e
 # linuxbrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
 PATH="$HOME/.linuxbrew/bin:$PATH"
+
 echo 'export PATH="$HOME/.linuxbrew/bin:$PATH"' >> ~/.bash_profile
+echo 'export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"' >>~/.bash_profile
+echo 'export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"' >>~/.bash_profile
 
 # brewtap
 brew tap Homebrew/bundle
