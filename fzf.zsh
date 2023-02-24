ba5b88e02da8f3eec6d59f1b98748a9b6cc82ffa
m.malkin <mmalkin@fun-box.ru>
2023-02-24 15:55:33 +0200

    updated

--- fzf.zsh
+++ fzf.zsh
@@ -1,20 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *$FZF_PATH/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
-fi
-
-# Man path
-# --------
-if [[ ! "$MANPATH" == *$FZF_PATH/man* && -d "$FZF_PATH/man" ]]; then
-  export MANPATH="$MANPATH:$FZF_PATH/man"
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$FZF_PATH/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "$FZF_PATH/shell/key-bindings.zsh"
-
+source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
--- install/install_macos.sh
+++ install/install_macos.sh
@@ -6,18 +6,19 @@ brew tap Homebrew/bundle
 (cd ~/.dotfiles && brew bundle -v)
 
 # oh-my-zsh
-(sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)")
+#(sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)")
 
 # link dotfiles
 rcup -U .oh-my-zsh/custom/plugins/pj -U scripts -U bin -x install
 
 # vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#vim +PluginInstall +qall
 
 # asdf
 asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 
 # projects
 mkdir -p $HOME/pjs
